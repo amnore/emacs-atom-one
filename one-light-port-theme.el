205bd76ec54df50b1b35df66e162c45c15a049c0
Chen Zhenge <Mr.ChenWithCapsule@outlook.com>
2021-07-13 00:51:42 +0800

    fix bugs

--- one-light-port-theme.el
+++ one-light-port-theme.el
@@ -33,10 +33,10 @@ If WEIGHT is not specified, default to 0.5."
 (defun one-light-port--luma (color)
   "Calculate the luma of COLOR."
   (let* ((rgb (apply 'color-hsl-to-rgb color))
-         (gamma-correction (lambda (u) (if (< u 0.3928)
+         (gamma-expand (lambda (u) (if (< u 0.3928)
                                            (/ u 12.92)
                                          (expt (/ (+ u 0.055) 1.055) 2.4))))
-         (rgb-linear (mapcar gamma-correction rgb)))
+         (rgb-linear (mapcar gamma-expand rgb)))
     (cl-reduce '+ (cl-mapcar (lambda (w u) (* w u))
                              '(0.2126 0.1752 0.0722)
                              rgb-linear))))
@@ -61,11 +61,13 @@ If WEIGHT is not specified, default to 0.5."
   "Convert hex COLOR to hsl, each component can have 1, 2 or 4 digits."
   (let* ((digits (/ (length color) 3))
          (hex-max (- (expt 16.0 digits) 1))
-         (subhex (lambda (pos) (substring color
-                                     (+ 1 (* digits pos))
-                                     (+ 1 (* digits (+ 1 pos)))))))
+         (subhex (lambda (pos) (string-to-number
+                           (substring color
+                                      (+ 1 (* digits pos))
+                                      (+ 1 (* digits (+ 1 pos))))
+                           16))))
     (apply 'color-rgb-to-hsl
-           (mapcar (lambda (x) (/ (string-to-number x) hex-max))
+           (mapcar (lambda (x) (/ x hex-max))
                    (mapcar subhex '(0 1 2))))))
 
 (defun one-light-port--color-contrast (color dark light &optional threshold)
